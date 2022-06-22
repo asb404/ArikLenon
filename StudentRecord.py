8b943ffd085792dec57962208ac6904935f9be54
Antara Bhavsar <75797827+asb404@users.noreply.github.com>
2022-06-22 11:13:00 +0530

    Record method Added to StudentRecord.py

--- StudentRecord.py
+++ StudentRecord.py
@@ -1,9 +1,21 @@
-'''An attendance record for a student can be represented as a string where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
-
-'A': Absent.
-'L': Late.
-'P': Present.
-Any student is eligible for an attendance award if they meet both of the following criteria:
-
-The student was absent ('A') for strictly fewer than 2 days total.
-The student was never late ('L') for 3 or more consecutive days.'''
+def Record(self, n: int) -> int:
+        # dptotal[i] the number of rewardable records without A whose lenghth is i
+        dptotal = [0] *  (n + 1) 
+        dp1,dp2,dp3 = 1,1,0
+        # dp1: the number of rewardable records that end with one L and without A
+        # dp2: the number of rewardable records that end with one P and without A
+        # dp3: the number of rewardable records that end with two Ls and without A
+        dptotal[0] = 1 # 
+        dptotal[1] = dp1 + dp2 + dp3
+        mod = 10**9 +7
+        for i in range(2,n+1):
+            dp1,dp2,dp3 = dp2 % mod,(dp1+dp2+dp3) % mod,dp1 % mod
+            dptotal[i] = (dp1 + dp2 + dp3) % mod
+        res = 0
+        res += dptotal[-1]
+        # take A into consideration: 
+        # A can take n location
+        for i in range(n):
+            # i is the location of A
+            res += (dptotal[n-i-1] * dptotal[i]) % mod
+        return res 
